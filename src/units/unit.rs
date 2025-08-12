703e55a4c3d3593c9038e2558329d9f426dcca85
ascii-16 <mail.akhil99@gmail.com>
2025-08-12 17:39:19 +0530

    refactor: add comments

--- src/units/unit.rs
+++ src/units/unit.rs
@@ -1,11 +1,16 @@
 pub trait Unit: Sized + Clone + std::fmt::Display + PartialEq + 'static {
     const ALL: &'static [Self];
 
+    /// Logic to convert to the base unit
+    /// Eg: For length, converting to meters
     fn to_base(&self, value: f64) -> f64;
 
+    /// Logic to coverting from the base unit
     fn from_base(&self, value: f64) -> f64;
 
     fn convert(&self, value: f64, to: &Self) -> f64 {
+        // Take any unit as base (Eg: meters in length)
+        // Then convert value to base unit first and then to desired unit
         let in_base = self.to_base(value);
         to.from_base(in_base)
     }
